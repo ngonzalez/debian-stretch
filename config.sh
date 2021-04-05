8f095e1a35c4425aa3c36fe29ec81b698190ba97
Nicolas Gonzalez <nicolasgonzalez180@gmail.com>
2021-04-05 14:41:16 +0200

    Remove deploy recipes, it's present in debian-sid repository

--- config.sh
+++ config.sh
@@ -1,7 +1,7 @@
-export NAMESPACE='effilab'
+export NAMESPACE='hebe'
 export PROJECT_NAME='hebe-299715'
 export SERVICE_ACCOUNT='service-account@hebe-299715.iam.gserviceaccount.com'
-export KEY_FILE="$HOME/Sites/ansible/hebe-299715-fed8d8f694a1.json"
+export KEY_FILE="$HOME/.hebe-299715-fed8d8f694a1.json"
 export IMAGE_TAG='debian-stretch'
 export USER='debian'
 export HOST_KEY=$(cat ~/.ssh/id_rsa.pub)
